1012cf3ce57b6f050a338b7b5fb30245f9946285
Cory Weller <cory.a.weller@gmail.com>
2023-02-13 14:05:08 -0500

    add bash wrapper

---
+++ ProtPipe.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+singularity_vers='3'
+
+# Check singularity exists
+if ! command -v singularity &> /dev/null; then
+    echo "INFO: singularity command not found, looking for singularity module"
+    if ! command -v module &> /dev/null; then
+        echo "ERROR: module command not found. Did you mean to run this on an HPC?"
+        exit 1
+    else
+        if $(module avail singularity/${singularity_vers} 2>&1 >/dev/null | grep -q 'No module'); then
+            echo 'ERROR: singularity cannot be found. Recheck installation?'
+            exit 1
+        else
+            echo 'INFO: module singularity found'
+            module load singularity/${singularity_vers}
+        fi
+    fi
+else
+    echo 'INFO: singularity command found'
+fi
+
+# Check python3 exists
+if ! command -v python3 &> /dev/null; then
+    if command -v module &> /dev/null; then
+        if $(module avail python/3 2>&1 >/dev/null | grep -q 'No module'); then
+            echo "ERROR: python3 module does not exist. Recheck installation?"
+            exit 1
+        else
+            module load python/3
+        fi
+    fi
+else
+    echo "INFO: python3 command found"
+fi
+
+python3 src/dia-nn.py $@

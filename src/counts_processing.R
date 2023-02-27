1f2fc410d23194858de09f2eee2bddee2df01937
Cory Weller <cory.a.weller@gmail.com>
2023-02-27 13:30:21 -0500

    finalize QC options

--- src/counts_processing.R
+++ src/counts_processing.R
@@ -19,6 +19,119 @@ options(warn = defaultW)    # Turn warnings back on
 #### DEFAULTS ######################################################################################
 intensity_min_threshold <- 0
 
+
+#### ARG PARSING ###################################################################################
+library(optparse)
+pwd = getwd()
+optparse_indent = '\n                '
+option_list = list( 
+    make_option(
+        "--pgfile",
+        default=NULL,
+        help=paste(
+            'Input file of Protein Group Intensity (from DIA-NN or Spectronaut)',
+            'Required.',
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--out",
+        dest="outdir",
+        default='output', 
+        help=paste(
+            'Directory to direct all output. Directory will be created if does not exist).',
+            'Defaults to the current working directory:',
+            pwd,
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--base",
+        dest="log_base",
+        default=10, 
+        help='Base for log transformation of intensity data. Default: 10'
+    ),
+    make_option(
+        "--normalize",
+        default='median',
+        type='character',
+        help=paste(
+            'shift: adjust sample intensities to match global median by adding a constant',
+            'scale: adjust sample intensities to match global median by multiplicative scaling',
+            'none: do not normalize',
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--sds",
+        dest = 'sds',
+        default=3,
+        type='numeric',
+        help=paste(
+            'Filter out samples with protein group counts > N standard deviations from the mean.',
+            'Default: 3',
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--imputation",
+        action = 'store_true',
+        default=FALSE, 
+        type='logical',
+        help=paste(
+            'Applies data imputation. Not yet implimented.',
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--design",
+        default=NULL,  
+        help=paste(
+            'Comma- or tab-delimited, three-column text file specifying the experimental design.',
+            'File should contain headers. Header names do not matter; column order DOES matter.',
+            'Columns order: <sample_name> <condition> <control>',
+            sep=optparse_indent
+        )
+    ),
+    make_option(
+        "--dry",
+        action = 'store_true',
+        default=FALSE, 
+        type='logical',
+        help=paste(
+            'Applies data imputation. Not yet implimented.',
+            sep=optparse_indent
+        )
+    )
+)
+
+usage_string <- "Rscript %prog --pgfile [filename] --design [filename] [other options] "
+opt <- parse_args(OptionParser(usage = usage_string, option_list))
+print(opt)
+
+
+if(TRUE) {
+    opt <- list(); 
+    opt$pgfile <- 'output/report.pg_matrix.tsv'
+    opt$outdir <- 'output'
+    opt$design <- 'example/design_matrix.csv'
+    opt$log_base <- 2
+    opt$dry <- FALSE
+    opt$normalize <- 'shift'
+}
+
+
+if(opt$dry) {
+    cat("INFO: Quitting due to --dry run\n")
+    quit(status=0)
+}
+
+if(! opt$normalize %in% c('shift','scale','none')) {
+    cat("ERROR: --normalize must be 'shift' 'scale' or 'none'\n")
+    quit(status=1)
+}
+
+
 #### FUNCTIONS #####################################################################################
 # Within data.table `DT`, 
 # for `sdcols` specified columns, 
@@ -112,7 +225,7 @@ plot_pg_thresholds <- function(DT, output_dir, output_filename) {
             geom_line() +
             geom_point(shape=21, alpha=0.5) +
             theme_few() +
-            labs(x='Minimum Log2(Intensity) Threshold',
+            labs(x=paste0('Minimum Log[', opt$log_base, '](Intensity) Threshold'),
             y=paste0('N Protein Groups where Log[', opt$log_base, '](Intensity)> 0')) 
     cat(paste0('   -> ', output_dir, output_filename, '\n'))
     ggsave(g,
@@ -165,13 +278,25 @@ plot_density <- function(DT.original, output_dir, output_filename) {
     )
 }
 
-median_normalize_intensity <- function(DT.original) {
+shift_normalize_intensity <- function(DT.original) {
     DT <- copy(DT.original)
     # Get global median of intensity values
     global_median <- median(DT[, Intensity])
     DT[, 'sample_median' := median(Intensity), by=Sample]
     DT[, 'global_median' := global_median]
     DT[, 'NormInt' := Intensity - (sample_median - global_median)]
+    DT[, c('Intensity', 'sample_median', 'global_median') := NULL]
+    setnames(DT, 'NormInt', 'Intensity')
+    return(DT[])
+}
+
+scale_normalize_intensity <- function(DT.original) {
+    DT <- copy(DT.original)
+    # Get global median of intensity values
+    global_median <- median(DT[, Intensity])
+    DT[, 'sample_median' := median(Intensity), by=Sample]
+    DT[, 'global_median' := global_median]
+    DT[, 'NormInt' := Intensity * (global_median / sample_median)]
     DT[, c('Intensity', 'sample_median', 'global_median') := NULL]
     setnames(DT, 'NormInt', 'Intensity')
     return(DT[])
@@ -269,100 +394,6 @@ get_correlations <- function(DT.original) {
 
 
 
-#### ARG PARSING ###################################################################################
-library(optparse)
-pwd = getwd()
-optparse_indent = '\n                '
-option_list = list( 
-    make_option(
-        "--pgfile",
-        default=NULL,
-        help=paste(
-            'Input file of Protein Group Intensity (from DIA-NN or Spectronaut)',
-            'Required.',
-            sep=optparse_indent
-        )
-    ),
-    make_option(
-        "--out",
-        dest="outdir",
-        default=pwd, 
-        help=paste(
-            'Directory to direct all output. Directory will be created if does not exist).',
-            'Defaults to the current working directory:',
-            pwd,
-            sep=optparse_indent
-        )
-    ),
-    make_option(
-        "--base",
-        dest="log_base",
-        default=2, 
-        help='Base for log transformation of intensity data. Default: 2'
-    ),
-    make_option(
-        "--normalize",
-        action = 'store_true',
-        default=FALSE,
-        type='logical',
-        help=paste(
-            'Applies data normalization, assuming it has not yet been done. Not yet implimented.',
-            sep=optparse_indent
-        )
-    ),
-    make_option(
-        "--imputation",
-        action = 'store_true',
-        default=FALSE, 
-        type='logical',
-        help=paste(
-            'Applies data imputation. Not yet implimented.',
-            sep=optparse_indent
-        )
-    ),
-    make_option(
-        "--design",
-        default=NULL,  
-        help=paste(
-            'Comma- or tab-delimited, three-column text file specifying the experimental design.',
-            'File should contain headers. Header names do not matter; column order DOES matter.',
-            'Columns order: <sample_name> <condition> <control>',
-            sep=optparse_indent
-        )
-    ),
-    make_option(
-        "--dry",
-        action = 'store_true',
-        default=FALSE, 
-        type='logical',
-        help=paste(
-            'Applies data imputation. Not yet implimented.',
-            sep=optparse_indent
-        )
-    )
-)
-
-usage_string <- "Rscript %prog --pgfile [filename] --design [filename] [other options] "
-opt <- parse_args(OptionParser(usage = usage_string, option_list))
-print(opt)
-
-if(opt$dry) {
-    cat("INFO: Quitting due to --dry run\n")
-    quit(status=0)
-}
-
-if(FALSE) {
-    opt <- list(); 
-    opt$pgfile <- 'output/report.pg_matrix.tsv'
-    opt$outdir <- 'output'
-    opt$design <- 'example/design_matrix.csv'
-    opt$log_base <- 2
-    opt$dry <- FALSE
-    opt$normalize <- TRUE
-}
-
-
-
 #### MAKE DIRS #####################################################################################
 QC_dir <- paste0(opt$outdir, '/QC/')
 if(! dir.exists(QC_dir)){
@@ -384,28 +415,23 @@ if(! dir.exists(DA_dir)){
 
 tryTo(paste0('INFO: Reading input file ', opt$pgfile),{
     dat <- fread(opt$pgfile)
-}, paste0('ERROR: problem trying to load ', opt$pgfile, ', does it exist?')
-)
+}, paste0('ERROR: problem trying to load ', opt$pgfile, ', does it exist?'))
 
-tryTo(paste0('INFO: Normalizing data from ', opt$pgfile), {
+tryTo(paste0('INFO: Massaging data from ', opt$pgfile, ' into a common style format for processing'), {
     dat <- standardize_format(dat)
-}, 'ERROR: failed! Check for missing/corrupt headers?'
-)
+}, 'ERROR: failed! Check for missing/corrupt headers?')
 
 tryTo(paste0('INFO: Converting to long format'), {
     dat.long <- melt_table(dat)
-}, 'ERROR: failed! Check for missing/corrupt headers?'
-)
+}, 'ERROR: failed! Check for missing/corrupt headers?')
 
-tryTo(paste0('INFO: Applying Log[base', opt$log_base, '](value+1)','transformation to intensities'),{
+tryTo(paste0('INFO: Applying Log[base', opt$log_base, '](value+1) transformation to intensities'),{
     dat.long <- dat.long[, Intensity := log((Intensity + 1), base=opt$log_base)]
-},'ERROR: failed! Was your log base numeric and > 1?'
-)
+},'ERROR: failed! Was your log base numeric and > 1?')
 
 tryTo('INFO: Excluding all unquantified or zero intensities', {
     dat.long <- dat.long[! is.na(Intensity)][Intensity != 0]
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
 
 
@@ -418,29 +444,46 @@ dat.long[, Sample := factor(Sample, levels=increasing_levels)]
 
 tryTo(paste0('INFO: Applying Filter Log[', opt$log_base, '](Intensity) > ',intensity_min_threshold),{
     dat.long <- filter_intensity(dat.long, intensity_min_threshold)
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
-tryTo('INFO: Calculating median-normalized intensities',{
-    dat.long.normalized <- median_normalize_intensity(dat.long)
-}, 'ERROR: failed!'
-)
-
-tryTo('INFO: Plotting intensity distributions',{
+tryTo('INFO: Plotting intensity distribution',{
     plot_flip_beeswarm(dat.long, QC_dir, 'intensities.png', plot_title='Un-normalized intensities')
-    plot_flip_beeswarm(dat.long.normalized, QC_dir, 'intensities_normalized.png', plot_title='Median-normalized intensities')
     # plot_density(dat.long, QC_dir, 'intensity_density.png')
     # plot_density(dat.long.normalized, QC_dir, 'intensity_density_normalized.png')
-    dat.long <- dat.long.normalized
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
-tryTo('INFO: Re-generating wide format using normalized intensities',{
-    dat.long <- dat.long.normalized
-    dat <- dcast(dat.long, Protein_Group+Genes+First_Protein_Description~Sample, value.var='Intensity')
-}, 'ERROR: failed!'
-)
 
+## Normalization takes place by default, and can be modified with the --normalize flag. See opts.
+if (opt$normalize == 'none') {
+    cat('Skipping median-normalization due to --normalize none\n')
+} else {
+    if (opt$normalize == 'shift') {
+
+        tryTo('INFO: Calculating median-normalized intensities by shifting sample intensities',{
+            dat.long <- shift_normalize_intensity(dat.long)
+        }, 'ERROR: failed!')
+
+        tryTo('INFO: Plotting shift-normalized intensity distributions',{
+            plot_flip_beeswarm(dat.long, QC_dir, 'intensities_shift_normalized.png', plot_title='shift-normalized intensities')
+        }, 'ERROR: failed!')
+
+    } else if (opt$normalize == 'scale') {
+
+        tryTo('INFO: Calculating median-normalized intensities by scaling sample intensities',{
+            dat.long <- scale_normalize_intensity(dat.long)
+        }, 'ERROR: failed!')
+
+        tryTo('INFO: Plotting scale-normalized intensity distributions',{
+            plot_flip_beeswarm(dat.long, QC_dir, 'intensities_scale_normalized.png', plot_title='scale-normalized intensities')
+        }, 'ERROR: failed!')
+    }
+
+    tryTo('INFO: Re-generating wide table with normalized intensities',{
+        original_colorder <- colnames(dat)
+        dat <- dcast(dat.long, Protein_Group+Genes+First_Protein_Description~Sample, value.var='Intensity')
+        setcolorder(dat, original_colorder)
+    }, 'ERROR: failed!')
+}
 
 # pgcounts represents the distribution of Protein Groups with Intensity > 0
 # Visually, it is represented as a bar plot with x=sample, y=N, ordered by descending N
@@ -451,8 +494,8 @@ tryTo('INFO: Tabulating protein group counts',{
     pgcounts[, Sample := factor(Sample, levels=pgcounts[order(-N), Sample])]
     ezwrite(pgcounts, QC_dir, 'protein_group_nonzero_counts.tsv')
     plot_pg_counts(pgcounts, QC_dir, 'protein_group_nonzero_counts.png')
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
+
 
 # pgthresholds represents the decay in number of unique protein groups per sample as
 # the minimum Intensity threshold is incremented. Visually represented as a line plot.
@@ -464,8 +507,7 @@ tryTo('INFO: Calculating protein group counts by minimum intensity thresholds',{
     }
     ezwrite(pgthresholds, QC_dir, 'protein_group_thresholds.tsv')
     plot_pg_thresholds(pgthresholds, QC_dir, 'protein_group_thresholds.png')
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
 
 
@@ -474,8 +516,7 @@ tryTo('INFO: Plotting sample intensity correlations',{
     dat.correlations <- get_correlations(dat)
     ezwrite(dat.correlations, QC_dir, 'sample_correlation.tsv')
     plot_correlation_heatmap(dat.correlations, QC_dir, 'sample_correlation.png')
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
 
 
@@ -483,91 +524,76 @@ tryTo('INFO: Importing experimental design',{
     design <- fread(opt$design)
     print(design[])
     conditions <- unique(design$condition)
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
 
-tryTo('INFO: ',{
+tryTo('INFO: running PCA',{
     # dcast long to wide
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
-tryTo('INFO: ',{
+tryTo('INFO: running UMAP ',{
 
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
 
-tryTo('INFO: ',{
+tryTo('INFO: performing clustering',{
 
-}, 'ERROR: failed!'
-)
+}, 'ERROR: failed!')
+
+
+
+## Exclude samples with N protein groups < opt$sds away from mean
+## Default value: 3 standard deviations, modifiable with --sds [N]
+tryTo('INFO: Identifying samples with protein group count outliers',{
+    cat(paste0('INFO: defining outliers as samples with [N protein groups] > ', opt$sds, ' standard deviations from the mean\n'))
+    stdev <- sd(pgcounts[,N])
+    mean_count <- mean(pgcounts[,N])
+    min_protein_groups <- floor(mean_count - (opt$sds * stdev))
+    max_protein_groups <- ceiling(mean_count + (opt$sds * stdev))
+    cat(paste0('INFO: Tolerating protein group counts in the range [', min_protein_groups,',',max_protein_groups,']'))
+    low_count_samples <- as.character(pgcounts[N < min_protein_groups, Sample])
+    high_count_samples <- as.character(pgcounts[N > max_protein_groups, Sample])
+    if(length(low_count_samples)==0) {
+        cat('\nINFO: No low group count samples to remove\n')
+    } else {
+        cat(paste0('\nINFO: Pruning low-count outlier ', low_count_samples))
+        cat('\n')
+        print(pgcounts[Sample %in% low_count_samples])
+        dat[, c(low_count_samples) := NULL]    # remove sample columns from wide table
+        dat.long <- dat.long[! (Sample %in% low_count_samples)] # remove rows from long table
+    }
+    if(length(high_count_samples)==0) {
+        cat('INFO: No high group count samples to remove\n')
+    } else {
+        cat(paste0('\nINFO: Pruning high-count outlier ', high_count_samples))
+        cat('\n')
+        print(pgcounts[Sample %in% high_count_samples])
+        dat[, c(high_count_samples) := NULL]    # remove sample columns from wide table
+        dat.long <- dat.long[! (Sample %in% high_count_samples)] # remove rows from long table
+    }
+}, 'ERROR: failed!')
 
 
 quit()
 
 
 
+#### CLUSTERING ####################################################################################
 
+dat[,-c(1:3)]
 
+## PCA
 
+## UMAP
 
-######### STOP HERE
-
-
-
-median_correlations <- foreach(i=conditions, .combine='rbind') %do% {
-    dt.corrs[SampleA %like% i | SampleB %like% i, list('Condition'=i, 'median_correlation'=median(Spearman))]
-}
-
+## CLUSTER
 
 
 
 
 
-# Filter by minimum Log2(Intensity) ?
 
-## Filter samples with protein group count 
+cluster_dir
 
-## Exclude samples with N protein groups < 3 SD away from mean
-sd(dat.long[Intensity > 0, .N, by=Sample][,N])
-
-
-
-#filter sample by the correlation,and protein number----------------------------
-##filter sample by the correlation
-condition=unique(gsub('_[1-9]*.mzML$','',rownames(cor_matrix)))
-median_cor=data.frame()
-for (i in condition) {
-  cor_i=cor_matrix[grep(i,rownames(cor_matrix)),grep(i,colnames(cor_matrix))]
-  cor_i_median=data.frame(apply(cor_i,2,median))
-  median_cor=rbind(median_cor,cor_i_median)
-}
-cor_filtter=rownames(median_cor)[median_cor$apply.cor_i..2..median. <= 0.9]
-
-##filter sample by the protein number
-pro_number_filtter=df_pro_num$sample[df_pro_num$pro.num <= mean(df_pro_num$pro.num)-2000]
-
-##sample filter
-sample_filter=union(cor_filtter,pro_number_filtter)
-if (length(sample_filter)>0) {
-  pro=pro[,-which(colnames(pro) %in% sample_filter)]
-  write.csv(pro,file = paste0(opt$outdir,"/",opt$prefix,"pro_intensity_fillter.csv"))
-}
-
-#normalization------------------------------------------------------------------
-if (!is.null(opt$n)){
-  pro[,grep('mzML',colnames(pro))]=apply(pro[,grep('mzML',colnames(pro))],2,
-                                        function(x) x*10^(round(median(df_pro_dis$value)))/(median(na.omit(x))))
-  write.csv(pro,file= paste0(opt$outdir,"/",opt$prefix,"pro_median_nor.csv"))
-}
-
-#proteomics data cluster--------------------------------------------------------
-##mkdir
-
-if (!dir.exists(paste0(opt$outdir,"/cluster_plot/"))){
-  dir.create(paste0(opt$outdir,"/cluster_plot/"),recursive = T)
-}
-out_dir=paste0(opt$outdir,"/cluster_plot/")
 
 ##cluster data(na=0)
 cluster_data=pro[,grep('mzML',colnames(pro))]
